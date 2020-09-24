fd3bb75e2348958f72c3e8d4e6c16170db0fd2f1
Hyeon <Hyeon@DESKTOP-6HJ3UK5>
2020-09-25 02:31:20 +0900

    # mr03 member id,pw,취미 get/set 추가

--- testgit/src/com/member/model/vo/Member.java
+++ testgit/src/com/member/model/vo/Member.java
@@ -5,4 +5,57 @@ public class Member {
 	private String userId;
 	private String userPw;
 	private String[] hobby;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String[] getHobby() {
+		return hobby;
+	}
+	public void setHobby(String[] hobby) {
+		this.hobby = hobby;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
