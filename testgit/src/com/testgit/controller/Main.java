5980d6cb4de4d203d437d81ec2f17a6adbcb4a58
Hyeon <Hyeon@DESKTOP-6HJ3UK5>
2020-09-25 02:13:39 +0900

    # test01 member에서 만든 String타입

--- testgit/src/com/testgit/controller/Main.java
+++ testgit/src/com/testgit/controller/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("마스터에서 만든 프린트7번줄");
+		
+		String member = "맴버에서 만든 스트링타입 9번줄";
 	}
 
 }
